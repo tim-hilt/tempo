fcc742ef5abf9a7673fead85043b76351ecc1f2f
Tim Hilt <thilt@in-technology.eu>
2022-10-31 20:58:34 +0100

    main: Adjust descriptions

--- cmd/get.go
+++ cmd/get.go
@@ -12,15 +12,10 @@ import (
 // getCmd represents the get command
 var getCmd = &cobra.Command{
 	Use:   "get",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Parent command for getting several other metrics",
+	Long:  "Parent command for getting several other metrics",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("get called")
+		fmt.Println("Standalone call not functional")
 	},
 }
 
--- cmd/ticketsForDay.go
+++ cmd/ticketsForDay.go
@@ -12,13 +12,8 @@ import (
 // ticketsForDayCmd represents the ticketsForDay command
 var ticketsForDayCmd = &cobra.Command{
 	Use:   "ticketsForDay",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Print all tickets for given day to the console",
+	Long:  "Print all tickets for given day to the console",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("Standalone call not functional")
 	},
