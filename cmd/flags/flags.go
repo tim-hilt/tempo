6d504e9b4df8fdec93fdab784a5b528a6d70b793
Tim Hilt <thilt@in-technology.eu>
2022-11-02 14:38:13 +0100

    main: Cleaned up flags.go

--- cmd/flags/flags.go
+++ cmd/flags/flags.go
@@ -1,17 +1,5 @@
 package flags
 
-import (
-	"os"
-	"time"
-
-	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/log"
-)
-
-func init() {
-	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339})
-}
-
 var (
 	Loglevel       int
 	DailyWorkhours int
