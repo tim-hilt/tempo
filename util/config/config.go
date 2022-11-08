84732e0c95d762d1c35d18ae2dba7acfbee27917
Tim Hilt <thilt@in-technology.eu>
2022-11-08 11:25:22 +0100

    main: Reuse config-funcs

--- util/config/config.go
+++ util/config/config.go
@@ -27,7 +27,15 @@ func GetHost() string {
 }
 
 func GetNotesdir() string {
-	return viper.GetString(NOTESDIR_CONFIG_VAL)
+	notesDir := viper.GetString(NOTESDIR_CONFIG_VAL)
+	if strings.HasPrefix(notesDir, "~") {
+		homedir, err := os.UserHomeDir()
+		if err != nil {
+			log.Fatal().Err(err).Msg("can't get users homedir")
+		}
+		notesDir = homedir + notesDir[1:]
+	}
+	return notesDir
 }
 
 func GetWorkhours() int {
@@ -39,29 +47,18 @@ func GetLoglevel() int {
 }
 
 func Validate() {
-	notesDir := formatNotesDir(viper.GetString(NOTESDIR_CONFIG_VAL))
+	user, password := GetCredentials()
 	config := configParams{
-		User:           viper.GetString(USER_CONFIG_VAL),
-		Password:       viper.GetString(PASSWORD_CONFIG_VAL),
-		JiraHost:       viper.GetString(HOST_CONFIG_VAL),
-		Notesdir:       notesDir,
-		Loglevel:       viper.GetInt(LOGLEVEL_CONFIG_VAL),
-		DailyWorkhours: viper.GetInt(DAILYWORKHOURS_CONFIG_VAL),
+		User:           user,
+		Password:       password,
+		JiraHost:       GetHost(),
+		Notesdir:       GetNotesdir(),
+		Loglevel:       GetLoglevel(),
+		DailyWorkhours: GetWorkhours(),
 	}
 	validate := validator.New()
 	err := validate.Struct(config)
 	if err != nil {
 		log.Fatal().Err(err).Msg("error when validating config-params")
 	}
-}
-
-func formatNotesDir(notesDir string) string {
-	if strings.HasPrefix(notesDir, "~") {
-		homeDir, err := os.UserHomeDir()
-		if err != nil {
-			log.Fatal().Err(err).Msg("error when formating notesDir")
-		}
-		return homeDir + notesDir[1:]
-	}
-	return notesDir
 }
