9c7cc5ad0a54f4c37eab014afa2274fe1bba7a94
Tim Hilt <thilt@in-technology.eu>
2022-12-13 13:51:34 +0100

    main: Add set methods

--- util/set/set.go
+++ util/set/set.go
@@ -6,8 +6,12 @@ type Set[T comparable] struct {
 	items map[T]bool
 }
 
-func New[T comparable]() Set[T] {
-	return Set[T]{items: make(map[T]bool)}
+func New[T comparable](is ...T) Set[T] {
+	s := Set[T]{items: make(map[T]bool)}
+	for _, i := range is {
+		s.Add(i)
+	}
+	return s
 }
 
 func (s *Set[T]) Delete(item T) {
@@ -45,4 +49,29 @@ func (s Set[T]) String() string {
 	}
 	str = str[:len(str)-2] + "}"
 	return str
+}
+
+func (s Set[T]) Cardinality() int {
+	return len(s.items)
+}
+
+func (s1 Set[T]) Union(s2 Set[T]) Set[T] {
+	newSet := New[T]()
+	for item := range s1.items {
+		newSet.Add(item)
+	}
+	for item := range s2.items {
+		newSet.Add(item)
+	}
+	return newSet
+}
+
+func (s1 Set[T]) Intersect(s2 Set[T]) Set[T] {
+	newSet := New[T]()
+	for item := range s1.items {
+		if s2.Contains(item) {
+			newSet.Add(item)
+		}
+	}
+	return newSet
 }
